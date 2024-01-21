039ef8568e0e38705230bf93159ff50af3518689
Laycawn <olalekan36@gmail.com>
2024-01-20 22:32:35 -0500

    final changes

--- assignment1.js
+++ assignment1.js
@@ -1,6 +1,15 @@
-console.log('WEB700 - Web Server Simulator');
+/*********************************************************************************
+*  WEB700 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: __Olalekan Jimoh ___ Student ID: __156555229___ Date: ____20/01/2024____
+*
+********************************************************************************/ 
 
-'use strict';
+console.log('Web Server Simulator');
+
 
 // Predefined arrays
 var serverVerbs = ["GET", "GET", "GET", "POST", "GET", "POST"];
@@ -8,7 +17,7 @@ var serverPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout"];
 var serverResponses = [
     "Welcome to WEB700 Assignment 1",
     "This assignment was prepared by Olalekan Yusau Jimoh",
-    "[Olalekan]: [oyjimoh@myseneca.ca]",
+    "Olalekan Yusau Jimoh: oyjimoh@myseneca.ca",
     "User Logged In",
     "Main Panel",
     "Logout Complete"
@@ -25,36 +34,28 @@ function httpRequest(httpVerb, path) {
 }
 
 // Manual Testing
-console.log(httpRequest("GET", "/contact")); // Should return "200: Welcome to WEB700 Assignment 1"
+console.log(httpRequest("POST", "/login")); // Should return "200: User Logged In"
 console.log(httpRequest("GET", "/about")); // Should return "200: This assignment was prepared by Olalekan Jimoh"
+console.log(httpRequest("PUT", "/")); // shows "404: Unable to process PUT request for /"
 
 
-// Additional arrays for testing
-var testVerbs = ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"];
-var testPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout", "/invalid", "/test"];
+
 
 // Utility function to generate random integers
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-// Function to automate tests with truncation after a set number of tries
+// Function to automate tests
 function automateTests() {
-    var testCount = 0;
-    var maxTests = 10; // Set the maximum number of tests here
-
+    var testVerbs = ["GET", "POST"];
+    var testPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout", "/randomPath1", "/randomPath2"];
     function randomRequest() {
-        if (testCount >= maxTests) {
-            clearInterval(testInterval);
-            console.log('Automated testing completed.');
-            return;
-        }
-
+        
         var randomVerb = testVerbs[getRandomInt(testVerbs.length)];
         var randomPath = testPaths[getRandomInt(testPaths.length)];
         var response = httpRequest(randomVerb, randomPath);
         console.log(`Request: ${randomVerb} ${randomPath} - Response: ${response}`);
-        testCount++;
     }
 
     var testInterval = setInterval(randomRequest, 1000); // Calls randomRequest every second
